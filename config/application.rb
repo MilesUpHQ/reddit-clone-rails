02e90aba1487152f98a100678501216ec9f87a55
mithun30052001 <lmithun01@gmail.com>
2022-10-26 23:53:46 +0530

    The errors related to reply are fixed

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,20 @@ module Discussions
     config.load_defaults 6.1
     config.exceptions_app = self.routes
 
+
     # Configuration for the application, engines, and railties goes here.
-    #
+     
+    config.after_initialize do
+      ActionText::ContentHelper.allowed_attributes.add 'style'
+      ActionText::ContentHelper.allowed_attributes.add 'controls'
+      ActionText::ContentHelper.allowed_attributes.add 'poster'
+
+      ActionText::ContentHelper.allowed_tags.add 'video'
+      ActionText::ContentHelper.allowed_tags.add 'audio'
+      ActionText::ContentHelper.allowed_tags.add 'source'
+      ActionText::ContentHelper.allowed_tags.add 'embed'
+      ActionText::ContentHelper.allowed_tags.add 'iframe'
+    end
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
