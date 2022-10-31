285a0040cf929e4c2e208385274f3f8f91f4ed5a
tushar R <tusharrravindran@gmail.com>
2022-10-31 21:22:00 +0530

    css files for pagination and preetier+

--- app/javascript/trix-editor-overrides.js
+++ app/javascript/trix-editor-overrides.js
@@ -4,11 +4,11 @@
 //     alert("File attachment not supported!")
 //   })
 
-window.addEventListener("trix-file-accept", function(event) {
-    const acceptedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/bmp', 'application/pdf', 'application/zip', 'application/vnd.rar', 'application/msword', 'application/vnd.ms-excel' ,'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
-    const maxFileSize = 1024 * 1024 
-    if (!acceptedTypes.includes(event.file.type) || event.file.size > maxFileSize) {
-      event.preventDefault()
-      alert("File type/size not supported")  
-    }
-  })     
+window.addEventListener("trix-file-accept", function (event) {
+  const acceptedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/bmp', 'application/pdf', 'application/zip', 'application/vnd.rar', 'application/msword', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document']
+  const maxFileSize = 1024 * 1024
+  if (!acceptedTypes.includes(event.file.type) || event.file.size > maxFileSize) {
+    event.preventDefault()
+    alert("File type/size not supported")
+  }
+})     
