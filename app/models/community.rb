a88cc0a3ceab70c32f2a47284583a1d79a35a3bf
Vasanthakumar <vasanthakumara117@gmail.com>
2022-11-25 21:37:47 +0530

    added categories list in model

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,9 +1,11 @@
 class Community < ApplicationRecord
   belongs_to :account
-  validates_presence_of :url, :name, :rules
+  validates_presence_of :url, :name, :rules, :category
   has_many :posts, dependent: :destroy
   has_many :subscriptions
   has_many :subscribers, through: :subscriptions, source: :account
   has_one_attached :profile_image
   has_one_attached :cover_image
+
+  CATEGORIES = ["Sports","Gaming","Technology","News","TV","Music","Crypto","Fasion","Food","Health","Science","Finance"]
 end
