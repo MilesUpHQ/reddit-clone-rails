00a9db14371053ffcf31af14f93c1a451ec5ec33
mithun30052001 <lmithun01@gmail.com>
2022-12-20 18:17:50 +0530

    publish

--- app/admin/communities.rb
+++ app/admin/communities.rb
@@ -1,3 +1,32 @@
 ActiveAdmin.register Community do
   permit_params :account_id, :name, :url, :summary, :rules, :total_members
+
+  index do
+    column :account_id
+    column :name
+    column :url
+    column :summary
+    column :rules
+    column :total_members
+    column :post_count_this_week
+    column :category
+    column :created_at
+    column :updated_at
+    actions
+  end
+  form do |f|
+    
+    f.inputs "Create Community" do
+    f.input :account
+    f.input :name
+    f.input :url
+    f.input :summary
+    f.input :rules
+    f.input :post_count_this_week
+    f.select :category, Category.pluck(:name), { :selected => params[:category] }
+    f.input :created_at
+    f.input :updated_at
+    f.submit "Submit", disable_with: 'Submiting...'
+  end
+end
 end
