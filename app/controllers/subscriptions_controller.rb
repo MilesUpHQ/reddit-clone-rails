6b541944113f783fbf0db8edf4a9aea75b9db791
Vasanthakumar <vasanthakumara117@gmail.com>
2022-12-22 10:41:33 +0530

    leave community feature added

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,11 +1,13 @@
 class SubscriptionsController < ApplicationController
   before_action :community_list
   before_action :authenticate_account!
+
   def create
     @subscription = Subscription.new(subscription_params)
     @subscription.account_id = current_account.id
     @subscription.save
-    redirect_to community_path(@subscription.community_id)
+    redirect_back(fallback_location: root_path)
+    # redirect_to community_path(@subscription.community_id)
   end
 
   def subscription_params
@@ -13,12 +15,23 @@ class SubscriptionsController < ApplicationController
   end
 
   def index
-    @subscriptions = Subscription.where(account_id: current_account.id) 
+    @subscriptions = Subscription.where(account_id: current_account.id)
   end
 
+  def destroy
+    @subscriptions = Subscription.where(account_id: current_account.id).where(community_id: params[:id])
+    @subscriptions.each do |subscription|
+      subscription.destroy
+    end
+    redirect_back(fallback_location: root_path)
+  end
 
   private
-  def community_list 
-    @communities = Community.order(created_at: :asc)  
+  def community_list
+    @communities = Community.order(created_at: :asc)
+  end
+
+  def find_subscription
+    @subscription = Subscription.find(params[:id])
   end
 end
