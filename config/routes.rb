c49c054cf030383f4094c5581e21de5e8f04dd61
mithun30052001 <lmithun01@gmail.com>
2022-12-22 18:30:43 +0530

    View of the drafts for a particular user is created both as a pop up and a seperate page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   resources :comments, only: [:create]
 
   post "post/vote" => "votes#create"
-
+  get '/draft', to: 'posts#draft'
   # devise_scope :account do
   #   get '/sign_in' => 'devise/sessions#new' # custom path to login/sign_in
   #   get '/sign_up' => 'devise/registrations#new', as: 'new_account_registration' # custom path to sign_up/registration
