94534c8bca9112c0f41187b556f58df55f34de20
mithun30052001 <81811776+mithun30052001@users.noreply.github.com>
2022-12-23 16:10:42 +0530

    bug in the admin category selection in community resolved (#9)
    * bug in the admin category selection in community resolved
    
    * option id bug is rectified and now shown with category id which is unique

--- app/admin/communities.rb
+++ app/admin/communities.rb
@@ -22,7 +22,7 @@ ActiveAdmin.register Community do
       f.input :summary
       f.input :rules
       f.input :post_count_this_week
-      f.input :category,  :label => 'category', :as => :select, :collection => Category.pluck(:name), :selected => params[:category] 
+      f.input :category,  :label => 'category', :as => :select, :collection => Category.all.collect {|option| [option.name, option.id]}, :selected => params[:category] 
       f.input :created_at
       f.input :updated_at
       f.submit "Submit", disable_with: 'Submiting...'
