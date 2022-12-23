dfed6aa92c357583baf459a55e8352ee114acba2
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-12-23 21:03:30 +0530

    Form is created for nested comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,17 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new comment_params
+    @comment = Comment.new comment_params  
     @comment.account_id = current_account.id
-
-    respond_to do |format|
-      format.js {
-        if @comment.save
-          @comments = Comment.where(post_id: @comment.post_id)
-          render "comments/create"
-        else
-          # unable to save
-        end
-      }
-    end
+    if @comment.save
+      redirect_to community_post_path(@comment.post_id, @comment.post), flash: {success: "comment was created successfully!" }
+    else
+      redirect_to community_post_path(@comment.post_id, @comment.post), flash: { danger: "comment was not saved!" }
+   end
   end
 
   def comment_params
-    params.require(:comment).permit(:message, :post_id)
+    params.require(:comment).permit(:message, :post_id, :replies, :parent, :parent_id)
   end
 
 end
