1428d51e02e64864a0e7961d277c702b1bbfe732
tushar R <tusharrravindran@gmail.com>
2022-12-23 10:33:11 +0530

    back to top script function

---
+++ app/javascript/includes/backtotop.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  var mybutton = $('#myBtn');
+
+  $(window).scroll(function() {
+    scrollFunction();
+  });
+
+  function scrollFunction() {
+    if ($(document).scrollTop() > 20) {
+      mybutton.show();
+    } else {
+      mybutton.hide();
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    $('html, body').animate({scrollTop: 0}, 'slow');
+  }
+});
