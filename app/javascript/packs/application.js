d0f998d34483d5cc434592f6e819505bed8f8692
tushar R <tusharrravindran@gmail.com>
2022-12-23 13:06:07 +0530

    back to top

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 require("bootstrap")
 
 require("../includes/vote.js")
+require("../includes/backtotop.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
