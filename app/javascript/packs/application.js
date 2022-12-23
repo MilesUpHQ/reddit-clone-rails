5d70bf276911a0d05e8d8191e703ad038cae385d
Vasanthakumar <vasanthakumara117@gmail.com>
2022-12-23 15:23:32 +0530

    navbar js is moved to seprate file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require("bootstrap")
 
 require("../includes/vote.js")
 require("../includes/infinitescroll.js")
+require("../includes/navbar.js")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
