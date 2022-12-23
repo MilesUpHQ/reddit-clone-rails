e7b02e49ce06f86b2441eeb48ccffb56d3d7b6c2
Srinandhini M <srinandhini@sedintechnologies.com>
2022-12-23 14:15:48 +0530

    Column created for saving post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_20_094452) do
+ActiveRecord::Schema.define(version: 2022_12_23_063418) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "first_name"
@@ -133,6 +133,8 @@ ActiveRecord::Schema.define(version: 2022_12_20_094452) do
     t.integer "total_comments", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
+    t.boolean "saved"
     t.index ["account_id"], name: "index_posts_on_account_id"
     t.index ["community_id"], name: "index_posts_on_community_id"
   end
