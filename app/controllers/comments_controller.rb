3ac81f6dab21f7d78f13259121457387a665f50e
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-12-26 12:46:07 +0530

    Corrections are added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,8 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new comment_params  
     @comment.account_id = current_account.id
-    if @comment.save
-      redirect_to community_post_path(@comment.post_id, @comment.post), flash: {success: "comment was created successfully!" }
-    else
-      redirect_to community_post_path(@comment.post_id, @comment.post), flash: { danger: "comment was not saved!" }
-   end
+    @comment.save
+    redirect_to community_post_path(@comment.post_id, @comment.post)
   end
 
   def comment_params
