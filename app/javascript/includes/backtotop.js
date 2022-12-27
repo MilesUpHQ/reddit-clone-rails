b26aad2d84a0d529dbe0ad6b53025e1564273eec
Vasanthakumar <vasanthakumara117@gmail.com>
2022-12-27 20:38:52 +0530

    changed all js ids to class to reuse js code

--- app/javascript/includes/backtotop.js
+++ app/javascript/includes/backtotop.js
@@ -1,20 +1,15 @@
 $( document ).on('turbolinks:load', function() {
-  var mybutton = $('#myBtn');
+  let mybutton = $('.top_btn');
 
   $(window).scroll(function() {
-    scrollFunction();
-  });
-
-  function scrollFunction() {
     if ($(document).scrollTop() > 20) {
       mybutton.show();
     } else {
       mybutton.hide();
     }
-  }
+  });
 
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
+  mybutton.click(function () {
     $('html, body').animate({scrollTop: 0}, 'slow');
-  }
+  });
  });
