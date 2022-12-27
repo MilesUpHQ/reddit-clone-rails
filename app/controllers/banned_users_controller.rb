1f938621a3d217653e2a740a55432daff0b7f67b
mithun30052001 <lmithun01@gmail.com>
2022-12-27 22:13:31 +0530

    Account_id issue in the banned users resolved

--- app/controllers/banned_users_controller.rb
+++ app/controllers/banned_users_controller.rb
@@ -1,14 +1,15 @@
 class BannedUsersController < ApplicationController
     def create 
         @banneduser = BannedUser.new(banneduser_params)
-        @username= Account.where(username: params[:username])
-        # @banneduser.community_id = params[:community_id]
-        # @banneduser.account_id = @username.id            
+        @username = Account.find_by!(username: @banneduser[:username])
+        @banneduser.community_id = Community.find_by!(params[:community_id]).id
+        @banneduser.account_id = @username.id  
+        # @banner_user.account_id = @username.id  
         @banneduser.save 
     end
     
       def banneduser_params
-        params.require(:banned_user).permit(:username, :reason, :explanation, :community_id)    
+        params.require(:banned_user).permit(:username, :reason, :explanation, :account_id, :community_id)    
     end
     
 end
