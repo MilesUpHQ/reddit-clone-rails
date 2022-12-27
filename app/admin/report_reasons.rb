38877328b4901a000cf2d80b2fc8a06e340532b3
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-12-27 23:05:48 +0530

    Admin side features are added to create report categories

---
+++ app/admin/report_reasons.rb
@@ -0,0 +1,22 @@
+ActiveAdmin.register ReportReason do
+    permit_params :report_category_id, :reason
+    index do 
+        column :report_category_id
+        column :report_category
+        column :reason 
+        column :created_at
+        column :updated_at
+        actions 
+    end
+
+    form do |f|
+        f.inputs "Report Reasons" do
+          f.input :report_category,  :label => 'report_category', :as => :select, :collection => ReportCategory.all.collect {|option| [option.name, option.id]}, :selected => params[:report_category]
+          f.input :reason
+          f.input :created_at
+          f.input :updated_at
+          f.submit "Submit", disable_with: 'Submiting...'
+        end
+      end
+end
+  
