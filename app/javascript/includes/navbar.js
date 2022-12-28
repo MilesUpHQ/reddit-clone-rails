3711d3c7c7451f29bdad4cde36e88af88bd6ddff
Vasanthakumar A <81355826+vasanthakumar-a@users.noreply.github.com>
2022-12-28 10:33:29 +0530

    Testing/seed (#28)

--- app/javascript/includes/navbar.js
+++ app/javascript/includes/navbar.js
@@ -1,9 +1,9 @@
 $( document ).on('turbolinks:load', function() {
-    $("#myInput_mycommunities").keydown(function () {
-        input = $('#myInput_mycommunities');
+    $(".myInput_mycommunities").keydown(function () {
+        input = $('.myInput_mycommunities');
         var input, filter, ul, li, a, i;
         filter = input.val().toUpperCase();
-        div = $('#myDropdown');
+        div = $('.myDropdown');
         a = div.find('a');
         a.each(function (index, element) {
             txtValue = $(element).text().toUpperCase();
@@ -25,7 +25,7 @@ $( document ).on('turbolinks:load', function() {
     const community_link = $('.community-nav-link')
     function link_community() {
         if (community_link.length) {
-            community_link.removeClass('active')    
+            community_link.removeClass('active')
             $(this).addClass('active')
         }
     }
