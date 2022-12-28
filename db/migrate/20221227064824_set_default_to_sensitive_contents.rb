136c6768666805a6864329d90c0f96f0afd30ab9
Srinandhini M <srinandhini@sedintechnologies.com>
2022-12-28 11:37:32 +0530

    Migration files corrected

--- db/migrate/20221227064824_set_default_to_sensitive_contents.rb
+++
@@ -1,7 +0,0 @@
-class SetDefaultToSensitiveContents < ActiveRecord::Migration[6.1]
-  def change
-    change_column :posts, :oc, :boolean, :default => false
-    change_column :posts, :spoiler, :boolean, :default => false
-    change_column :posts, :nsfw, :boolean, :default => false
-  end
-end
