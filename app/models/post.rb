f45a1ad9d1896b3ab14e1a9f0fe546b5734ad5b5
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2022-12-29 17:59:57 +0530

    bug is fixed for post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   belongs_to :community
   validates_presence_of :title, :body, :account_id, :community_id
   has_many :comments
-  belongs_to :report
+  has_many :report
   has_rich_text :body 
   has_many_attached :images
 
