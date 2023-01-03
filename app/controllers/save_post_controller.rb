582596f15f0d71f83cc9284fa3b68af0d55c5088
Vasanthakumar A <81355826+vasanthakumar-a@users.noreply.github.com>
2023-01-03 23:16:56 +0530

    Issue/save unsave toggle ajax (#84)
    * login page designed
    
    * signup and edit page designed
    
    * forget and confirm page designed
    
    * error message added for login and signup
    
    * validates added for image, accept only images
    
    * helper added for error message
    
    * login signup card size increased
    
    * save unsave toggle done using ajax
    
    * save unsave toggle done using ajax
    
    * manifest added in gitignore
    
    * save post url refactored

--- app/controllers/save_post_controller.rb
+++ app/controllers/save_post_controller.rb
@@ -2,23 +2,32 @@ class SavePostController < ApplicationController
   before_action :authenticate_account!
 
   def create
-  end
+    set_saved_post
+    if @saved_post.nil?
+      @saved_post = SavePost.new(
+        account_id: current_account.id,
+        post_id: params[:id]
+      )
+      @saved_post.save
+      @is_saved = true
+    else
+      @saved_post.destroy
+      @is_saved = false
+    end
 
-  def save
-    @saved_post = SavePost.new(
-      account_id: current_account.id,
-      post_id: params[:id]
-     )
-    @saved_post.save!
-  end
-  
-  def unsave
-      @saved_post=SavePost.find_by(account_id: current_account.id,  post_id: params[:id])
-       @saved_post.destroy
-       
+    respond_to do |format|
+      format.js {
+        @post_id = params[:id]
+        render "save_post/create"
+      }
+    end
   end
 
   private
+
+  def set_saved_post
+    @saved_post = SavePost.find_by(account_id: current_account.id,  post_id: params[:id])
+  end
 
   def save_post_params
     params.require(:save).permit(:id)
