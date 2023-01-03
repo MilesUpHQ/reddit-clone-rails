51554717263a0c64b933621f0cd4d6b710e21fea
Aboorva1 <abookannan567@gmail.com>
2023-01-03 17:15:12 +0530

    Report creation error is now solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,3 +41,25 @@ end
     is_drafted: false,
   )
 end
+
+%w[Spam Copyright Hate Impersonation Harassment Misinformation].each do |category|
+  ReportCategory.create!(name: category)
+end
+["Harmful Bots","Unsolicited Messaging"].each do |reason|
+  ReportReason.create!(
+    report_category_id: 1,
+    reason: reason
+  )
+end
+["Yours or an individual","Someone else's"].each do |reason|
+ReportReason.create!(
+  report_category_id: 2,
+  reason: reason
+)
+end
+["You","Someone else"].each do |reason|
+ReportReason.create!(
+  report_category_id: 5,
+  reason: reason
+)
+end
