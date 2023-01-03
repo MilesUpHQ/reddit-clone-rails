97afb7e25fc5ee55fcb608ee8cb2a31be955c01f
Srinandhini M <srinandhini@sedintechnologies.com>
2023-01-03 15:39:09 +0530

    Multiple images fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   
   belongs_to :account
   belongs_to :community
-  validates_presence_of :title, :body, :account_id, :community_id
+  validates_presence_of :title, :body,:images,:account_id, :community_id
   has_many :comments
   has_many :reports
   has_many :save_posts
