77af08d41ca2d52a6ec1ef534df34a34463c0d21
mithun30052001 <lmithun01@gmail.com>
2023-01-04 16:38:03 +0530

    Error messages are added for both community and signup forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+    def error_msg(resource, field)
+        if resource.errors.any?
+          resource.errors.full_messages_for(field).each do |error_message|
+            return error_message
+          end
+        end
+        ""
+      end
+    
+      def is_error(resource, field)
+        if resource.errors.any?
+          resource.errors.full_messages_for(field).each do |error_message|
+            return " border-danger"
+          end
+        end
+      end
+
+    #   def error_msg_community field
+    #     if @community.errors.any?
+    #         @community.errors.full_messages_for(field).each do |error_message|
+    #         return error_message
+    #       end
+    #     end
+    #     ""
+    #   end
+    
+    #   def is_error_community field
+    #     if @community.errors.any?
+    #         @community.errors.full_messages_for(field).each do |error_message|
+    #         return " border-danger"
+    #       end
+    #     end
+    #   end
 end
