fd354709a5273b8797281b51811c58e6cf417ec9
mithun30052001 <lmithun01@gmail.com>
2023-01-04 16:44:37 +0530

    unwanted comments removed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,20 +16,4 @@ module ApplicationHelper
         end
       end
 
-    #   def error_msg_community field
-    #     if @community.errors.any?
-    #         @community.errors.full_messages_for(field).each do |error_message|
-    #         return error_message
-    #       end
-    #     end
-    #     ""
-    #   end
-    
-    #   def is_error_community field
-    #     if @community.errors.any?
-    #         @community.errors.full_messages_for(field).each do |error_message|
-    #         return " border-danger"
-    #       end
-    #     end
-    #   end
-end
+  end 
