6b002e3f90b1d228db2a6370536b03004449e222
Vasanthakumar A <81355826+vasanthakumar-a@users.noreply.github.com>
2023-01-05 15:22:52 +0530

    issue/profile picture and cover photo validated (#124)
    * profile picture and cover photo validated
    
    * added concers for image validation
    
    * concern module name changed and refactored

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   extend FriendlyId
-  friendly_id :title ,use: %i[slugged history finders] 
-  
+  friendly_id :title ,use: %i[slugged history finders]
+  include ImageValidation
+
   belongs_to :account
   belongs_to :community
   validates_presence_of :account_id, :community_id
@@ -10,10 +11,11 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :reports, dependent: :destroy
   has_many :save_posts
-  has_rich_text :body 
+  has_rich_text :body
   has_many_attached :images, dependent: :destroy
+  validate :acceptable_image
   scope :drafts, ->(account_id) { where(account_id: account_id, is_drafted: true) }
-  
+
   def score
     if self.upvotes > 0 || self.downvotes > 0
       self.upvotes > 0 ? (self.upvotes - self.downvotes) : (self.downvotes * -1)
@@ -21,8 +23,13 @@ class Post < ApplicationRecord
       0
     end
   end
-  
+
   def should_generate_new_friendly_id?
     title_changed? || slug.blank?
   end
-end    
+
+  def acceptable_image
+    validate_multiple_images(images, :images)
+  end
+
+end
