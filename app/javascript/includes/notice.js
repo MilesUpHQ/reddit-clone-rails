6ea5052d506d826ea397e83812b4104b454a16c4
mithun30052001 <lmithun01@gmail.com>
2023-01-05 10:55:27 +0530

    note.js changed from js to jquery

--- app/javascript/includes/notice.js
+++ app/javascript/includes/notice.js
@@ -1,9 +1,8 @@
-
 $(document).on('turbolinks:load', function () {
   setTimeout(function () {
-     document.getElementById('notice').style.display = 'none';
-    }, 3000); 
-     document.getElementById('notice-close').onclick = function () {
-     document.getElementById('notice').style.display = 'none';
-  }
-});  
+    $('#notice').hide();
+  }, 3000);
+  $('#notice-close').click(function () {
+    $('#notice').hide();
+  });
+});
