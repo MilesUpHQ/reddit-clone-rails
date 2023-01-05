84d3b6cbb7ae5106a5e80cbf8d0f87456b405142
mithun30052001 <81811776+mithun30052001@users.noreply.github.com>
2023-01-05 12:00:35 +0530

    Fix/customize error messages (#114)
    * Error messages are added for both community and signup forms
    
    * unwanted comments removed
    
    * error messages are shown for title and rich text body in post
    
    * Automatic community selection in draft link clicking  done

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,19 +1,19 @@
 module AccountsHelper
 
-  def error_msg field
+  def error_message field
     if resource.errors.any?
       resource.errors.full_messages_for(field).each do |error_message|
         return error_message
       end
     end
     ""
-  end
+  end 
 
-  def is_error field
+  def is_errors field
     if resource.errors.any?
       resource.errors.full_messages_for(field).each do |error_message|
         return " border-danger"
-      end
+      end 
     end
   end
 
