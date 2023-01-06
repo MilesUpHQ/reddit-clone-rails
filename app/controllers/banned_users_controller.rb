dd5abcac35223f853389546fe624d593b02ee5a8
tushar R <tusharrravindran@gmail.com>
2023-01-06 18:41:58 +0530

    resolve quote to :

--- app/controllers/banned_users_controller.rb
+++ app/controllers/banned_users_controller.rb
@@ -4,23 +4,23 @@ class BannedUsersController < ApplicationController
     @community = Community.find_by!(params[:community_id])
     @username = Account.find_by(username: @banned_user[:username])
     if @username.nil?  
-        flash[:alert] = t("user_not_present")
+        flash[:alert] = t(:user_not_present, username: @username.username)
     else  
         @subscriptions = Subscription.where(account_id: @username.id,community_id: params[:banned_user][:community_id])
         if @subscriptions.any?
             @banned_user.account_id = @username.id  
             if @banned_user.account_id == current_account.id
-                flash[:notice] = t("admin_ban?")
+                flash[:notice] = t(:admin_ban, username: @username.username)
             else
                 if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil? 
-                    flash[:notice] = t(:banned_successfully)
+                    flash[:notice] = t(:banned_successfully, username: @username.username)
                     @banned_user.save
                 elsif BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).present?
-                    flash[:alert] = t(:already_banned)
+                    flash[:alert] = t(:already_banned, username: @username.username)
                 end   
             end 
         else    
-            flash[:alert] = t(:not_present_to_ban)
+            flash[:alert] = t(:not_present_to_ban, username: @username.username)
         end 
     end
     redirect_to mod_path(@community) and return
