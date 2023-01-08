51e21bd61bbab0a3225a08f46430e2eb2369c31f
tushar R <tusharrravindran@gmail.com>
2023-01-08 22:30:59 +0530

    username issue resolved

--- app/controllers/banned_users_controller.rb
+++ app/controllers/banned_users_controller.rb
@@ -3,26 +3,21 @@ class BannedUsersController < ApplicationController
     @banned_user = BannedUser.new(banned_user_params)
     @community = Community.find_by(params[:community_id])
     @username = Account.find_by(username: @banned_user[:username])
-    return flash[:alert] = t(:user_not_present, username: @username.username) if @username.nil?
+    return flash[:alert] = t(:user_not_present, username: @banned_user[:username]) if @username.nil?
     return flash[:notice] = t(:admin_ban, username: @username.username) if @username == current_account
-    
     @subscriptions = Subscription.where(account_id: @username.id, community_id: params[:banned_user][:community_id])
     return flash[:alert] = t(:not_present_to_ban, username: @username.username) unless @subscriptions.any?
-    
     @banned_user.account_id = @username.id  
     if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil? 
       flash[:notice] = t(:banned_successfully, username: @username.username)
       @banned_user.save
     else
       flash[:alert] = t(:already_banned, username: @username.username)
-    end
-    
+    end    
     redirect_to mod_path(@community) and return
   end
-  
-  
+    
   def banned_user_params
     params.require(:banned_user).permit(:username, :reason, :explanation, :account_id, :community_id)  
-  end
-  
+  end  
 end
