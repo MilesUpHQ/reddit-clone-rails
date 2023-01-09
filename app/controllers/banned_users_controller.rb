89df725a8a63f94e6e41c758529985c88c9caa1a
tushar R <57782379+tusharravindran@users.noreply.github.com>
2023-01-09 17:05:37 +0530

    Issue/mod tools UI (#152)
    * feature flash notification added
    
    * require packages added
    
    * mod UI
    
    * .
    
    * ISSUE at Banned Uers ,Only Joined Members Banned with Flash Notice
    
    * indentation resolved
    
    * Banned User Issue Ban anyone not in Community Resolved
    
    * resolve quote to :
    
    * comments reolved
    
    * refactored code
    
    * code refactored
    
    * username issue resolved
    
    * reolved code working
    
    * ode refactored
    
    * code aligned
    
    Co-authored-by: Vasanthakumar <vasanthakumara117@gmail.com>
    Co-authored-by: mithun30052001 <lmithun01@gmail.com>

--- app/controllers/banned_users_controller.rb
+++ app/controllers/banned_users_controller.rb
@@ -1,19 +1,41 @@
 class BannedUsersController < ApplicationController
-    def create
-        @banned_user = BannedUser.new(banned_user_params)
-        @username = Account.find_by!(username: @banned_user[:username])
-        @banned_user.account_id = @username.id
-        if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil?
-            @banned_user.save
-            flash[:notice] = t("banned.admin", username: @username.username)
-            redirect_to mod_path(Community.find_by(params[:community_id]))
-        else
-            redirect_to mod_path(Community.find_by(params[:community_id])), alert: "The user '"+@username.username.to_s+"' is already banned!."
-        end
-    end
+  before_action :set_banned_users,:set_community, only: [:create]
+  before_action :set_username, only: [:create]
+  before_action :is_admin?, only: [:create]
+  def create  
+    redirect_to mod_path(@community) and return flash[:alert] = t(:not_present_to_ban, username: @username.username) unless @subscriptions.any?
+    @banned_user.account_id = @username.id  
+    if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil? 
+      flash[:notice] = t(:banned_successfully, username: @username.username)
+      @banned_user.save
+    else
+      flash[:alert] = t(:already_banned, username: @username.username)
+    end    
+    redirect_to mod_path(@community) and return
+  end
+    
+  def banned_user_params
+    params.require(:banned_user).permit(:username, :reason, :explanation, :account_id, :community_id)  
+  end  
 
-    def banned_user_params
-     params.require(:banned_user).permit(:username, :reason, :explanation, :account_id, :community_id)
-    end
+  private
 
+  def set_banned_users
+    @banned_user = BannedUser.new(banned_user_params)
+  end
+
+  def set_community
+    @community = Community.find_by(params[:community_id])
+  end
+
+  def set_username
+    @username = Account.find_by(username: @banned_user[:username])
+    redirect_to mod_path(@community) and return flash[:alert] = t(:user_not_present, username: @banned_user[:username]) if @username.nil?
+    @subscriptions = Subscription.where(account_id: @username.id, community_id: params[:banned_user][:community_id])
+  end
+ 
+  def is_admin?
+    redirect_to mod_path(@community) and return flash[:notice] = t(:admin_ban, username: @username.username) if @username == current_account
+  end 
+  
 end
