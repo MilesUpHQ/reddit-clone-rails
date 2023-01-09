951149e4af0b25d94404bd6050607e20fc819195
tushar R <tusharrravindran@gmail.com>
2023-01-09 18:31:36 +0530

    refactor flah.en.ym naming for banned controller

--- app/controllers/banned_users_controller.rb
+++ app/controllers/banned_users_controller.rb
@@ -3,13 +3,13 @@ class BannedUsersController < ApplicationController
   before_action :set_username, only: [:create]
   before_action :is_admin?, only: [:create]
   def create  
-    redirect_to mod_path(@community) and return flash[:alert] = t("banned.not_present", username: @username.username) unless @subscriptions.any?
+    redirect_to mod_path(@community) and return flash[:alert] = t("banned.user_not_present", username: @username.username) unless @subscriptions.any?
     @banned_user.account_id = @username.id  
     if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil? 
       flash[:notice] = t("banned.success", username: @username.username)
       @banned_user.save
     else
-      flash[:alert] = t("banned.already", username: @username.username)
+      flash[:alert] = t("banned.user_already_exists", username: @username.username)
     end    
     redirect_to mod_path(@community) and return
   end
@@ -30,12 +30,12 @@ class BannedUsersController < ApplicationController
 
   def set_username
     @username = Account.find_by(username: @banned_user[:username])
-    redirect_to mod_path(@community) and return flash[:alert] = t("banned.not_exists", username: @banned_user[:username]) if @username.nil?
+    redirect_to mod_path(@community) and return flash[:alert] = t("banned.user_not_exists", username: @banned_user[:username]) if @username.nil?
     @subscriptions = Subscription.where(account_id: @username.id, community_id: params[:banned_user][:community_id])
   end
  
   def is_admin?
-    redirect_to mod_path(@community) and return flash[:alert] = t("banned.admin", username: @username.username) if @username == current_account
+    redirect_to mod_path(@community) and return flash[:alert] = t("banned.is_admin", username: @username.username) if @username == current_account
   end 
   
 end
