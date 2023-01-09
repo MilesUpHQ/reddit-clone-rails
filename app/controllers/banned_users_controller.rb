b6f77b291bb4c08283f02be311048d3f3ae98b0d
tushar R <tusharrravindran@gmail.com>
2023-01-09 18:43:18 +0530

    refactor toastr name

--- app/controllers/banned_users_controller.rb
+++ app/controllers/banned_users_controller.rb
@@ -3,7 +3,7 @@ class BannedUsersController < ApplicationController
   before_action :set_username, only: [:create]
   before_action :is_admin?, only: [:create]
   def create  
-    redirect_to mod_path(@community) and return flash[:alert] = t("banned.user_not_present", username: @username.username) unless @subscriptions.any?
+    redirect_to mod_path(@community) and return flash[:alert] = t("banned.user_not_present_in_community", username: @username.username) unless @subscriptions.any?
     @banned_user.account_id = @username.id  
     if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil? 
       flash[:notice] = t("banned.success", username: @username.username)
@@ -30,7 +30,7 @@ class BannedUsersController < ApplicationController
 
   def set_username
     @username = Account.find_by(username: @banned_user[:username])
-    redirect_to mod_path(@community) and return flash[:alert] = t("banned.user_not_exists", username: @banned_user[:username]) if @username.nil?
+    redirect_to mod_path(@community) and return flash[:alert] = t("banned.user_not_available", username: @banned_user[:username]) if @username.nil?
     @subscriptions = Subscription.where(account_id: @username.id, community_id: params[:banned_user][:community_id])
   end
  
