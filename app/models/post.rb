c452f3a5bc97e0d6566843a6c73d04fbf5fd588f
SrinandhiniM <srinandhinim491@gmail.com>
2023-01-10 12:04:25 +0530

    images

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,8 +11,8 @@ class Post < ApplicationRecord
   belongs_to :account
   belongs_to :community
   validates_presence_of :account_id, :community_id
-  validates :title, presence: true
-  validates :body, presence: true
+ # validates :title, presence: { message: "Title can't be blank" }
+  #validates :body, presence: { message: "Body can't be blank" }
   has_many :comments, dependent: :destroy
   has_many :reports, dependent: :destroy
   has_many :save_posts
