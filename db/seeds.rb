30ea9165b5f3b6098e8eaabfda81056429ce5372
tushar R <tusharrravindran@gmail.com>
2023-01-10 12:21:09 +0530

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-AdminUser.create!(email: 'admin@reddit.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-AdminUser.create!(email: 'admin@example.com', password: '123456', password_confirmation: '123456') if Rails.env.development?
+AdminUser.create!(email: 'admin@reddit.com', password: '123456', password_confirmation: 'password') if Rails.env.development?
 
 # for these Accounts you need to confirmation token for logging in
 Account.create!(first_name: 'Vasanth', last_name: 'A', username: 'vasi', email: 'vasanthakumar@gmail.com', password: '123456', password_confirmation: '123456', confirmed_at: Time.now) if Rails.env.development?
