8129fffee5a243e095c4a1f3c647112f3e985d54
Aboorva1 <abookannan567@gmail.com>
2023-01-11 11:57:17 +0530

    Border danger appears only for error fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,14 @@
 module ApplicationHelper
     def error_msg(resource, field)
         if resource.errors.any?
-          return resource.errors.messages_for(field).first
+          return resource.errors.full_messages_for(field).first
         end
         ""
       end
 
       def style_error_class(resource, field, css)
-        if resource.errors.any?
-          if resource.errors.messages_for(field)
-            return (css+" border-danger").to_s
-          end
+        if resource.errors[field].any?  
+          return (css+" border-danger").to_s
         end
         return css.to_s
       end
