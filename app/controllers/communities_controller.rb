f8376af8525d6e8712dc79d22022825586b258fa
tushar R <tusharrravindran@gmail.com>
2023-01-16 15:22:46 +0530

    comments refactor resolved

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -20,14 +20,11 @@ class CommunitiesController < ApplicationController
     @community = Community.find(params[:id])
     @posts = @community.posts.limit(20).sort_by { |p| p.score }.reverse
     @subscriber_count = @community.subscribers.count
-    @subscribed = if account_signed_in?
-                    Subscription.where(community_id: @community.id,account_id: current_account.id).any?
-                  else
-                    false
-                  end
+    @subscribed = account_signed_in? ? Subscription.where(community_id: @community.id,account_id: current_account.id).any? : false
     @subscription = Subscription.new
     @banned_users = BannedUser.all
   end
+  
 
   def new
     @community = Community.new
