ff48e1e993156857fb429ff65746770ef3d2a665
Vasanthakumar <vasanthakumara117@gmail.com>
2023-01-17 23:34:38 +0530

    modified routes and helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-
   devise_for :admin_users, ActiveAdmin::Devise.config
-  #The database queries or associations are being executed during the application's boot process,
-  #before the database connection has been established, so rescue is used.
-  ActiveAdmin.routes(self) rescue ActiveAdmin::DatabaseHitDuringLoad
+  # The database queries or associations are being executed during the application's boot process,
+  # before the database connection has been established, so rescue is used.
+  begin
+    ActiveAdmin.routes(self)
+  rescue StandardError
+    ActiveAdmin::DatabaseHitDuringLoad
+  end
   devise_for :accounts
-  get "u/:username" => "public#profile", as: :profile
-  get "/saved_posts" => "save_post#saved_posts"
+  get 'u/:username' => 'public#profile', as: :profile
+  get '/saved_posts' => 'save_post#saved_posts'
   resources :communities, path: :r do
-    resources :posts, path: :p, except: [:new] do
+    resources :posts, path: :p do
       member do
         patch :close
         get :report
@@ -16,22 +19,21 @@ Rails.application.routes.draw do
     end
   end
 
-  get "/submit", to: "posts#new", as: "new_community_post"
+  get '/submit', to: 'posts#new', as: 'submit_new_community_post'
 
-
-  post 'save_post/:id', to: "save_post#create", as: "save_post_path"
+  post 'save_post/:id', to: 'save_post#create', as: 'save_post_path'
 
   resources :report_reasons
   resources :banned_users
-  resources :subscriptions, only: [:create, :destroy]
+  resources :subscriptions, only: %i[create destroy]
   resources :comments, only: [:create]
   resources :reports, only: [:create]
 
-  post "p/vote" => "votes#create"
+  post 'p/vote' => 'votes#create'
 
-  get 'r/:id/mod' , to: 'communities#mod' , as: 'mod'
+  get 'r/:id/mod', to: 'communities#mod', as: 'mod'
 
-  match "/404", to: "errors#not_found",via: :all
+  match '/404', to: 'errors#not_found', via: :all
   get :autocomplete, to: 'communities#autocomplete'
   root to: 'public#index'
 end
