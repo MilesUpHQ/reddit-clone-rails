9ec98d9642ffaf0ddfe5b4d2713cc5ebf42dae73
Aboorva1 <abookannan567@gmail.com>
2023-01-17 16:44:36 +0530

    Adjusted spacing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def error_msg(resource, field)
     if resource.errors.any?
       return resource.errors.full_messages_for(field).first
-      end
-      ""
     end
+    ""
+  end
 
   def style_error_class(resource, field, css)
     if resource.errors[field].any?  
