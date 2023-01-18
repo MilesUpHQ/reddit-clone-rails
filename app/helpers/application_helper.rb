aac01b487adccdb83636443f87a3e5f272171263
Aboorva1 <113507326+Aboorva1@users.noreply.github.com>
2023-01-18 11:51:48 +0530

    Bug/profile picture (#207)
    * Displayed profile image for community and account
    
    * Adjusted spacing
    
    * Renamed method as suggested

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,20 @@
 module ApplicationHelper
-    def error_msg(resource, field)
-        if resource.errors.any?
-          return resource.errors.full_messages_for(field).first
-        end
-        ""
-      end
-
-      def style_error_class(resource, field, css)
-        if resource.errors[field].any?  
-          return (css+" border-danger").to_s
-        end
-        return css.to_s
-      end
+  def error_msg(resource, field)
+    if resource.errors.any?
+      return resource.errors.full_messages_for(field).first
+    end
+    ""
   end
+
+  def style_error_class(resource, field, css)
+    if resource.errors[field].any?  
+      return (css+" border-danger").to_s
+    end
+    return css.to_s
+  end
+
+  def display_profile_picture resource, class_name, default_image
+    image_path = resource.profile_image.attached? ? resource.profile_image : default_image
+    image_tag(image_path, :class => class_name)
+  end
+end
