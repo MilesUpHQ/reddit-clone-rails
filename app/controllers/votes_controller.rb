172708b913c5b90d87d210244babf337d8128b7b
Aboorva1 <113507326+Aboorva1@users.noreply.github.com>
2023-01-18 11:51:23 +0530

    Changed votes logic from controller to model (#194)
    * Changes logic from controller to model
    
    * removed duplicates
    
    * Code refactored
    
    * refactored code in model as suggested

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,32 +2,17 @@ class VotesController < ApplicationController
   def create
     post_id = params[:post_id]
 
-    vote = Vote.new
-    vote.post_id = params[:post_id]
-    vote.upvote = params[:upvote]
-    vote.account_id = current_account.id
-
-    existing_vote = Vote.where(account_id: current_account.id, post_id: post_id)
-    @new_vote = existing_vote.size > 1
-
-    respond_to do |format|
-      format.js {
-        if existing_vote.size > 0
-          existing_vote.first.destroy
-        else
-          if vote.save
-            @success = true
-          else
-            @success = false
-          end
-        end
-
-        @post = Post.find(post_id)
-        @is_upvote = params[:upvote]
-
-      render "votes/create"
-    }
-    end
+    new_vote = Vote.new(
+      post_id: params[:post_id],
+      upvote: params[:upvote],
+      account_id: current_account.id
+    )
+    existing_vote = Vote.where(account_id: current_account.id, post_id: post_id) 
+    Vote.save_vote new_vote, existing_vote
+    @is_new_vote = existing_vote.size > 1
+    @post = Post.find(post_id)
+    @is_upvote = params[:upvote]
+    render "votes/create"
   end
 
   private
