35ff2dada83a5c1a5a11c7858a06b40804894c8f
Aboorva1 <abookannan567@gmail.com>
2023-01-19 15:05:44 +0530

    Removed hidden fields from reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,18 +1,20 @@
 class ReportsController < ApplicationController
   def create
-    report = Report.new (report_params)
-    if report.save
+    @post = Post.find(params[:post_id])
+    @report = @post.reports.build report_params
+    @report.account_id = current_account.id
+    if @report.save
       flash[:notice] = t("report.success")
     else
       flash[:alert] = t("report.error")
     end
     respond_to do |format|
-      format.html {  redirect_to community_post_path(report.post.community_id, report.post) }
+      format.html {  redirect_to community_post_path(@report.post.community_id, @report.post) }
       format.js
     end
   end
 
   def report_params
-    params.require(:report).permit(:post_id, :account_id, :report_category_id, :report_reason_id)
+    params.require(:report).permit(:report_category_id, :report_reason_id)
   end
 end
