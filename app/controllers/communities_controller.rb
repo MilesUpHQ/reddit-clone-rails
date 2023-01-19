b4b975e600edb11781263805a88eddc6bef4f1fa
Aboorva1 <abookannan567@gmail.com>
2023-01-19 17:35:30 +0530

    Changed name

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -50,7 +50,7 @@ class CommunitiesController < ApplicationController
   end
 
   def update
-    if @community.update(community_values)
+    if @community.update(community_params)
       redirect_to @community, notice: t('community.updated')
     else
       flash[:alert] = t('form.required')
