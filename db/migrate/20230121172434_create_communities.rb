3d280b902e5b7f679d663dd851652dfd611bedcf
Vasanthakumar <vasanthakumara117@gmail.com>
2023-01-21 23:47:30 +0530

    added post model

--- db/migrate/20230121172434_create_communities.rb
+++ db/migrate/20230121172434_create_communities.rb
@@ -1,7 +1,7 @@
 class CreateCommunities < ActiveRecord::Migration[6.1]
   def change
     create_table :communities do |t|
-      t.reference :account
+      t.references :account, null: false, foreign_key: true
       t.string :name
       t.string :url
       t.string :rules
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_121_172_434) do
-  create_table 'accounts', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'username'
-    t.text 'bio'
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_accounts_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_accounts_on_reset_password_token', unique: true
+ActiveRecord::Schema.define(version: 2023_01_21_180123) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.text "bio"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
-  create_table 'communities', force: :cascade do |t|
-    t.integer 'account_id'
-    t.string 'name'
-    t.string 'url'
-    t.text 'rules'
-    t.integer 'total_members'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['account_id'], name: 'index_communities_on_account_id'
+  create_table "communities", force: :cascade do |t|
+    t.integer "account_id", null: false
+    t.string "name"
+    t.string "url"
+    t.string "rules"
+    t.integer "total_members"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_communities_on_account_id"
   end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "account_id", null: false
+    t.integer "community_id", null: false
+    t.string "title"
+    t.text "body"
+    t.integer "upvotes", default: 0
+    t.integer "downvotes", default: 0
+    t.integer "total_comments", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_posts_on_account_id"
+    t.index ["community_id"], name: "index_posts_on_community_id"
+  end
+
+  add_foreign_key "communities", "accounts"
+  add_foreign_key "posts", "accounts"
+  add_foreign_key "posts", "communities"
 end
