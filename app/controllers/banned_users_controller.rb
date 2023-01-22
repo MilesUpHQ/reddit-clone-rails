31238abc30854eb3e528d892ed90e418c41d2704
tushar R <tusharrravindran@gmail.com>
2023-01-22 23:48:15 +0530

    fixed

--- app/controllers/banned_users_controller.rb
+++ app/controllers/banned_users_controller.rb
@@ -2,40 +2,35 @@ class BannedUsersController < ApplicationController
   before_action :set_banned_users,:set_community, only: [:create]
   before_action :set_username, only: [:create]
   before_action :is_admin?, only: [:create]
-  def create  
+  def create
     redirect_to mod_path(@community) and return flash[:alert] = t("ban.user_not_present_in_community", username: @username.username) unless @subscriptions.any?
-    @banned_user.account_id = @username.id  
-    if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil? 
+    @banned_user.account_id = @username.id
+    if BannedUser.find_by(account_id: @banned_user.account_id, community_id: @banned_user.community_id).nil?
       flash[:notice] = t("ban.success", username: @username.username)
       @banned_user.save
     else
       flash[:alert] = t("ban.already_done", username: @username.username)
-    end    
+    end
     redirect_to mod_path(@community) and return
   end
-    
   def banned_user_params
-    params.require(:banned_user).permit(:username, :reason, :explanation, :account_id, :community_id)  
-  end  
-
+    params.require(:banned_user).permit(:username, :reason, :explanation, :account_id, :community_id)
+  end
   private
-
   def set_banned_users
     @banned_user = BannedUser.new(banned_user_params)
   end
-
   def set_community
     @community = Community.find_by(params[:community_id])
   end
-
   def set_username
-    @username = Account.find_by(username: @banned_user[:username])
+    @username=Account.find(params[:banned_user][:username])
+    #@username = Account.find_by(username: params[:username])
+    puts("======\n \n \n==========",@username.username)
     redirect_to mod_path(@community) and return flash[:alert] = t("ban.user_not_available", username: @banned_user[:username]) if @username.nil?
     @subscriptions = Subscription.where(account_id: @username.id, community_id: params[:banned_user][:community_id])
   end
- 
   def is_admin?
     redirect_to mod_path(@community) and return flash[:alert] = t("ban.moderator_not_possible", username: @username.username) if @username == current_account
-  end 
-  
+  end
 end
