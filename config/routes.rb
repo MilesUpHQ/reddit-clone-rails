20e56102e42a9f699894fb056f2002aa85fc12d7
Aboorva1 <abookannan567@gmail.com>
2023-01-30 12:45:27 +0530

    Routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       devise_for :accounts
       resources :communities
-      resources :posts
+      resources :posts do
+        resources :comments, only: [:create]
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
