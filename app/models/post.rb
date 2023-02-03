e49d03b95135b041e9bfffd829d497bca7e9f51a
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-03 06:27:50 +0530

    Vote model is created and association is added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :account
   belongs_to :community
+  has_many :votes, dependent: :destroy
 
   has_many :comments, dependent: :destroy
   validates_presence_of :title, :body
