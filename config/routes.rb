025908cabd224110cef323b1c343c45c206fea36
Vasanthakumar <vasanthakumara117@gmail.com>
2023-02-03 13:30:56 +0530

    Added routes to get joined accounts for community

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       devise_for :accounts
       resources :communities do
+        get 'joined_accounts', to: 'communities#community_joined_account'
         resources :banned_users
         resources :subscriptions
         resources :posts do
