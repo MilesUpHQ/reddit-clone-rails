81aec35a8e01637f6318e6ff1db48894c69d3e04
tushar R <tusharrravindran@gmail.com>
2023-02-03 18:06:42 +0530

    remov Validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,4 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
-  validates :body, presence: true
 end
