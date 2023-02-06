a54eecfadb36d4343de35abb11b510a639852554
SrinandhiniM <srinandhinim491@gmail.com>
2023-02-06 10:48:35 +0530

    code refactored

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,6 +1,7 @@
 class PublicController < ApplicationController
   include SavePostHelper
-
+  before_action :saved_posts, only: [:profile]
+  before_action :subscriptions, only: [:profile]
   def index
     @communities = Community.order("post_count_this_week desc").limit(5)
     @posts = Post.order(created_at: :desc).page(params[:page]).per 5
@@ -10,21 +11,35 @@ class PublicController < ApplicationController
   def profile
     redirect_to new_account_session_path  unless account_signed_in? 
     community_list
-    @saved_posts =  account_signed_in? ? SavePost.where(account_id: current_account.id) : []
-    @subscriptions = account_signed_in? ? Subscription.where(account_id: current_account.id) : []
     @community = Community.find(@subscriptions.pluck(:community_id))
     @my_communities = account_signed_in? ? Community.where(account_id: current_account.id) : []
     @profile = Account.find_by_username params[:username]
     @posts = @profile.posts
     @my_comments = Comment.where(account_id: @profile.id)
-    @my_posts = Post.where(account_id: @profile.id)
-    @hot_myposts = Post.where(account_id: @profile.id).order(view_count: :desc).page(params[:page]).per 5
+    @my_posts = my_posts
+    @hot_myposts = hot_posts.page(params[:page]).per 5
    end
-
-
 
   def my_comments
     @my_comments = Comment.where(account_id: current_account.id).pluck(:message).with_rich_text_content.order(created_at: :asc)
   end
+
+  private
+  def saved_posts
+    @saved_posts =  account_signed_in? ? SavePost.where(account_id: current_account.id) : []
+  end
+
+  def subscriptions
+    @subscriptions = account_signed_in? ? Subscription.where(account_id: current_account.id) : []
+  end
+
+  def my_posts
+    Post.where(account_id: @profile.id)
+  end
+  
+  def hot_posts
+    Post.where(account_id: @profile.id).order(view_count: :desc)
+  end
+  
 end
 
