597d80259b999ac7f6e18c709f9ecd586f6b0695
tushar R <tusharrravindran@gmail.com>
2023-02-06 20:31:51 +0530

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_for :accounts
-      resources :users do
+      resources :accounts do
         resources :save_posts
       end
       resources :communities do
