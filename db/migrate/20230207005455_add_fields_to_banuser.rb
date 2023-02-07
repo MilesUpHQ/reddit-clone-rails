ca90b131c1098bbd2c589fa019700a39f51274f3
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-07 06:43:38 +0530

    Migration file alteres

--- db/migrate/20230207005455_add_fields_to_banuser.rb
+++ db/migrate/20230207005455_add_fields_to_banuser.rb
@@ -1,6 +1,6 @@
 class AddFieldsToBanuser < ActiveRecord::Migration[6.1]
   def change
-    add_column :banned_users, :duration, :integer, default: 0
+    add_column :banned_users, :duration, :integer
     add_column :banned_users, :permanent, :boolean
   end
 end
