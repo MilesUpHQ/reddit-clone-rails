e1c6da8d44f7df2ba4b2a63c1ff5592c7ebf9aed
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-07 11:58:18 +0530

    Error resolved for function perform_later

--- app/models/un_ban_job.rb
+++ app/models/un_ban_job.rb
@@ -1,7 +1,7 @@
 class UnBanJob < ApplicationJob
     queue_as :default
   
-    def perform(ban_user_id)
+    def perform_later(ban_user_id)
       ban_user = BanUser.find(ban_user_id)
       if  ban_user.duration > 0 && ban_user.days_since_creation >= ban_user.duration
         ban_user.destroy
