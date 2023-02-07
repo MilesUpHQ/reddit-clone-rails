9e743a4e33d2179a618b1983fc83b39c36c5f37d
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-07 06:31:40 +0530

    added a method to calculate the no.of days

--- app/models/banned_user.rb
+++ app/models/banned_user.rb
@@ -2,4 +2,8 @@ class BannedUser < ApplicationRecord
   validates_presence_of :reason, :explanation
   belongs_to :account
   belongs_to :community
+
+  def days_since_creation
+    (Time.now - created_at) / 86400
+  end
 end
