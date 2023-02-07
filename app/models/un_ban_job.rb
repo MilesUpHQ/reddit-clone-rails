9771317fc1980660249c112f96dfd7b54b0649f3
KSOWNDAR <sowndar.k.ece@sece.ac.in>
2023-02-07 06:42:47 +0530

    Handled a condition for default value

--- app/models/un_ban_job.rb
+++ app/models/un_ban_job.rb
@@ -3,7 +3,7 @@ class UnBanJob < ApplicationJob
   
     def perform(ban_user_id)
       ban_user = BanUser.find(ban_user_id)
-      if ban_user.days_since_creation >= ban_user.ban_duration
+      if  ban_user.duration > 0 && ban_user.days_since_creation >= ban_user.duration
         ban_user.destroy
       end
     end
